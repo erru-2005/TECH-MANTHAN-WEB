b0c212b58e0b7cbc589235c733355af7af9076ec
errol <errualmeida2005@gmail.com>
2025-08-18 23:42:31 +0530

    first commit

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template, request, jsonify
+from . import mongo
+from bson import ObjectId
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/admin/home')
+def admin_home():
+    return render_template('home.html')
+
